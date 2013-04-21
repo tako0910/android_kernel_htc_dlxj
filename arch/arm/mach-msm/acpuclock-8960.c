4eaeafabdb1764a4b556f4454661d4da50336b22
tako0910 <tako.oct0910@gmail.com>
2013-04-21 22:43:40 +0900

    remove/modify __cpuinit

--- arch/arm/mach-msm/acpuclock-8960.c
+++ arch/arm/mach-msm/acpuclock-8960.c
@@ -1389,7 +1389,7 @@ out:
 	return rc;
 }
 
-static void __cpuinit hfpll_init(struct scalable *sc, struct core_speed *tgt_s)
+static void hfpll_init(struct scalable *sc, struct core_speed *tgt_s)
 {
 	pr_debug("Initializing HFPLL%d\n", sc - scalable);
 
@@ -1409,7 +1409,7 @@ static void __cpuinit hfpll_init(struct scalable *sc, struct core_speed *tgt_s)
 	hfpll_enable(sc, 0);
 }
 
-static void __cpuinit regulator_init(int cpu, struct acpu_level *lvl)
+static void regulator_init(int cpu, struct acpu_level *lvl)
 {
 	int ret;
 	struct scalable *sc = &scalable[cpu];
@@ -1467,7 +1467,7 @@ static void __cpuinit regulator_init(int cpu, struct acpu_level *lvl)
 	sc->regulators_initialized = true;
 }
 
-static void __cpuinit init_clock_sources(struct scalable *sc,
+static void init_clock_sources(struct scalable *sc,
 				      struct core_speed *tgt_s)
 {
 	uint32_t regval;
@@ -1491,7 +1491,7 @@ static void __cpuinit init_clock_sources(struct scalable *sc,
 	sc->current_speed = tgt_s;
 }
 
-static void __cpuinit per_cpu_init(void *data)
+static void per_cpu_init(void *data)
 {
 	int cpu = smp_processor_id();
 
@@ -1552,7 +1552,7 @@ static void __init cpufreq_table_init(void) {}
 #endif
 
 #define HOT_UNPLUG_KHZ STBY_KHZ
-static int __cpuinit acpuclock_cpu_callback(struct notifier_block *nfb,
+static int acpuclock_cpu_callback(struct notifier_block *nfb,
 					    unsigned long action, void *hcpu)
 {
 	static int prev_khz[NR_CPUS];
@@ -1611,7 +1611,7 @@ static int __cpuinit acpuclock_cpu_callback(struct notifier_block *nfb,
 	return NOTIFY_OK;
 }
 
-static struct notifier_block __cpuinitdata acpuclock_cpu_notifier = {
+static struct notifier_block acpuclock_cpu_notifier = {
 	.notifier_call = acpuclock_cpu_callback,
 };
 
