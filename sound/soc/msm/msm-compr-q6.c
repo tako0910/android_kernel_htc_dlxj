57c739724cc39bc0fa7d7f5b522e9950318b2598
Ethan Chen <intervigil@gmail.com>
2013-06-06 14:36:28 -0400

    ASoC: msm: report correct number of compressed audio codecs

--- sound/soc/msm/msm-compr-q6.c
+++ sound/soc/msm/msm-compr-q6.c
@@ -548,8 +548,8 @@ static void populate_codec_list(struct compr_audio *compr,
 		struct snd_pcm_runtime *runtime)
 {
 	pr_debug("%s\n", __func__);
-	
-	compr->info.compr_cap.num_codecs = 1;
+	/* MP3 Block */
+	compr->info.compr_cap.num_codecs = 8;
 	compr->info.compr_cap.min_fragment_size = runtime->hw.period_bytes_min;
 	compr->info.compr_cap.max_fragment_size = runtime->hw.period_bytes_max;
 	compr->info.compr_cap.min_fragments = runtime->hw.periods_min;
